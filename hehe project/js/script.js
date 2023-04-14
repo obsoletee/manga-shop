3a343e62046be069acb69225c14f12bac73339fa
Ilyaz Mamedov <93796675+obsoletee@users.noreply.github.com>
2023-04-14 15:45:52 +0300

    Update script.js

--- hehe project/js/script.js
+++ hehe project/js/script.js
@@ -1,10 +1,1 @@
 "use strict";
-
-const navigationBurger = document.querySelector('.navigation__burger');
-const menuList = document.querySelector('.menu__list');
-
-function burgerClick (e) {
-	console.log(e);
-	menuList.classList.toggle('active');	
-}
-navigationBurger.addEventListener('click', burgerClick);
